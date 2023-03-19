828ea19add8efc3bbfb7e269415cde6419f65c07
clbrunet <brunet.clement5@gmail.com>
2023-03-19 13:45:13 +0100

    feat: add a nice free look 3rd person camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     private void Update()
+    {
+        UpdateCursor();
+        UpdateTransform();
+    }
+
+    private void UpdateCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+
+    private void UpdateTransform()
     {
         Vector2 inputVector = GetNormalizedInputVector();
         if (inputVector == Vector2.zero)
         {
             return;
         }
-        Vector3 direction = new(inputVector.x, 0f, inputVector.y);
+        Vector3 direction = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f) * new Vector3(inputVector.x, 0f, inputVector.y);
         const float ROTATE_SPEED = 10f;
         transform.forward = Vector3.Slerp(transform.forward, direction, ROTATE_SPEED * Time.deltaTime);
         const float SPEED = 3f;
-        transform.position += SPEED * Time.deltaTime * direction;
+        characterController.Move(SPEED * Time.deltaTime * direction);
     }
 
     private Vector2 GetNormalizedInputVector()
